d0e3c9a1e5596d49d4a12e843bff5c94b2ab0c25
Lizandra Debussy <lizandradebussy@hotmail.com>
2022-09-27 09:54:03 -0300

    Requisitos 2 e 3 concluídos

---
+++ desafio3.sql
@@ -0,0 +1,11 @@
+SELECT
+    u.nome_usuario AS usuario,
+    COUNT(h.cancoes_id) AS qt_de_musicas_ouvidas,
+    ROUND(SUM(c.duracao_segundos) / 60, 2) AS total_minutos
+FROM SpotifyClone.usuario AS u
+INNER JOIN SpotifyClone.historico AS h 
+ON u.usuario_id = h.usuario_id
+INNER JOIN SpotifyClone.cancoes AS c 
+ON c.cancoes_id = h.cancoes_id
+GROUP BY usuario
+ORDER BY usuario ASC;
